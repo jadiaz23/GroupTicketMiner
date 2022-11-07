ee5c3e00094a85d0e4e39a02a027d7c670bd2de0
Armando <jadiaz23@miners.utep.edu>
2022-11-07 16:36:59 -0700

    Fixed my issue

--- src/RunTicketMiner.java
+++ src/RunTicketMiner.java
@@ -2,6 +2,7 @@ public class RunTicketMiner {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("This is a test");
+        System.out.println("I fricking fixed it");
     }
 
 }
