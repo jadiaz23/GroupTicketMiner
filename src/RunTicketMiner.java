b06600234681d8c11ac210b5e4a2efe15ace870c
ayanez11 <70168024+ayanez11@users.noreply.github.com>
2022-11-07 16:22:09 -0700

    another test

--- src/RunTicketMiner.java
+++ src/RunTicketMiner.java
@@ -1,7 +1,7 @@
 public class RunTicketMiner {
     public static void main(String[] args){
         System.out.println("Hello world");
-        System.out.println("This is a test");
+        System.out.println("This is another test");
     }
 
 }
