12a24bc36d531a09e92af4e76472d73d765bfd79
ayanez11 <70168024+ayanez11@users.noreply.github.com>
2022-11-14 22:08:47 -0700

    added javaDoc folder

---
+++ JavaDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Admin"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"EventFactory"},{"p":"<Unnamed>","l":"openFiles"},{"p":"<Unnamed>","l":"RunTicketMiner"}]
